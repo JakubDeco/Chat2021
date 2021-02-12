4cae3bd8d42413a1f4e0d9fb7bc90c20895db8cf
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-02-12 10:52:49 +0100

    added login scene

--- src/sk/kosickaakademia/deco/sample/Controller.java
+++ src/sk/kosickaakademia/deco/sample/Controller.java
@@ -1,4 +1,18 @@
 package sk.kosickaakademia.deco.sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+    public TextField loginField;
+    public Button signInButton;
+    public Label failedSignInLabel;
+    public PasswordField passwordField;
+
+    public void clickSignIn(MouseEvent mouseEvent) {
+        System.out.println("huraaa");
+    }
 }
