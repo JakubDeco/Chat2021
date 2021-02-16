13ec2340cb6f8b858804faf2cce3c149faeefcd8
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-02-16 14:35:03 +0100

    connected main.fxml with MainController; added MainController.setLoginNameText

--- src/sk/kosickaakademia/deco/sample/MainController.java
+++ src/sk/kosickaakademia/deco/sample/MainController.java
@@ -1,7 +1,29 @@
 package sk.kosickaakademia.deco.sample;
 
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
 
+    public Label loginName;
+    public Button refreshBtn;
+    public TextArea inboxTextField;
+    public Button sendBtn;
+    public TextField recipient;
+    public TextField newMessage;
+
+    public void setLoginNameText(String name){
+        if (name == null || name.isBlank())
+            return;
+
+        loginName.setText(name);
+    }
+
+    public void sendMessage(MouseEvent mouseEvent) {
+
+    }
 }
